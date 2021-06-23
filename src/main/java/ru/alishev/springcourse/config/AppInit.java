6af0a2ce6831aa0d81eac939171a9c71f46d99c2
Stanislav Dusiak <stanislav.dusiak@gmail.com>
2021-06-23 12:23:59 +0300

    2.4.2

--- src/main/java/ru/alishev/springcourse/config/AppInit.java
+++ src/main/java/ru/alishev/springcourse/config/AppInit.java
@@ -7,17 +7,24 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 
 
-public class MySpringMvcDispatcherSerlvetIntitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
+    // Метод, указывающий на класс конфигурации
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        return null;
+        return new Class<?>[]{
+                WebConfig.class
+        };
     }
 
+    // Добавление конфигурации, в которой инициализируем ViewResolver, для корректного отображения jsp.
     @Override
     protected Class<?>[] getServletConfigClasses() {
-        return new Class[]{SpringConfig.class};
+        return new Class<?>[]{
+                WebConfig.class
+        };
     }
 
+    /* Данный метод указывает url, на котором будет базироваться приложение */
     @Override
     protected String[] getServletMappings() {
         return new String[]{"/"};
