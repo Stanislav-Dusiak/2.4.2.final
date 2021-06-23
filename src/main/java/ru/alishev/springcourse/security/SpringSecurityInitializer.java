f70b145224267251d1e7b27a8c78bdcc1ec3ee18
Stanislav Dusiak <stanislav.dusiak@gmail.com>
2021-06-23 12:24:22 +0300

    2.4.2

---
+++ src/main/java/ru/alishev/springcourse/security/SpringSecurityInitializer.java
@@ -0,0 +1,7 @@
+package ru.alishev.springcourse.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+    //пустой класс, использующийся для регистрации модуля в спринг-контейнере
+}
