8dcb763ff8004cfad9d1867f1e0e136fcb69ba88
Stanislav Dusiak <stanislav.dusiak@gmail.com>
2021-06-29 19:06:21 +0300

    problem1 is solved

--- src/main/java/ru/alishev/springcourse/dao/RoleDao.java
+++ src/main/java/ru/alishev/springcourse/dao/RoleDao.java
@@ -1,4 +1,8 @@
 package ru.alishev.springcourse.dao;
 
+import ru.alishev.springcourse.models.Role;
+
+
 public interface RoleDao {
+    Role getOne(Long aLong);
 }
---
+++ src/main/java/ru/alishev/springcourse/dao/RoleDaoImpl.java
@@ -0,0 +1,14 @@
+package ru.alishev.springcourse.dao;
+
+import org.springframework.stereotype.Component;
+import ru.alishev.springcourse.models.Role;
+
+
+@Component
+public class RoleDaoImpl implements RoleDao {
+
+    @Override
+    public Role getOne(Long aLong) {
+        return null;
+    }
+}
